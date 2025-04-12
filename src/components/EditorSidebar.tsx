9f16fee50aa0cd2855b7d8150ceba80ed1807b1e
Joel Chemmanur <joelchem@outlook.com>
2025-04-12 14:26:32 -0400

    modifying sidebar

---
+++ src/components/EditorSidebar.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { ArrowLeft } from "lucide-react";
+
+export function EditorSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+
+
+
+  return (
+    <Sidebar className="border-r-0" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuButton asChild>
+            <a href="/network">
+              <ArrowLeft/>
+              <span>View Workspaces</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Recent Boards
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <span>Test</span>
+                </SidebarMenuButton>  
+              </SidebarMenuItem>  
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -1,14 +1,13 @@
-import { AppSidebar } from "@/components/app-sidebar";
+import { EditorSidebar } from "@/components/EditorSidebar";
 import Canvas from "@/components/Canvas";
-import { NavActions } from "@/components/nav-actions";
 import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbPage } from "@/components/ui/breadcrumb";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-dropdown-menu";
 
 export default function EditorPage() {
-    return <SidebarProvider>
-        <AppSidebar>
-        </AppSidebar>
+    return <>
+        <SidebarProvider>
+        <EditorSidebar/>
         <SidebarInset>
             <header className="flex h-14 shrink-0 items-center gap-2">
                 <div className="flex flex-1 items-center gap-2 px-3">
@@ -29,4 +28,6 @@ export default function EditorPage() {
             </Canvas>
         </SidebarInset>
     </SidebarProvider>
+    </>
+    
 }
