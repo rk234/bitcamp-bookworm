05522894238b87095cca87d0cf2066c29731d429
Joel Chemmanur <joelchem@outlook.com>
2025-04-12 17:14:21 -0400

    basic network stuff

--- src/pages/NetworkPage.tsx
+++ src/pages/NetworkPage.tsx
@@ -1,5 +1,18 @@
+import NetworkGraph from "@/components/NetworkGraph"
 
 export default function NetworkPage() {
-
-    return <>Network</>;
-}
+    return (
+      <>
+        <div>
+          <div className="text-center p-8 w-full">
+            <span className="font-extrabold text-2xl">Welcome back Person!</span>
+            <div className="bg-secondary my-10 mx-60 rounded-md">
+              <NetworkGraph/>
+              <span>Workspace 1</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
