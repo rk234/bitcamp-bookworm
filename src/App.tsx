a7ccdc4148bd063693b2803c7f5ffa290461f483
Joel Chemmanur <joelchem@outlook.com>
2025-04-11 23:37:21 -0400

    pages
    mesg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import MarkdownRenderer from './components/MarkdownRenderer'
+import LandingPage from './pages/LandingPage'
+import EditorPage from './pages/EditorPage'
+import NetworkPage from './pages/NetworkPage'
 
 function App() {
   const md = `# Header\n\nSome text\n\n**Yay math**: $\\frac{1}{x}$
 `
   return (
     <Routes>
-      <Route index element={<MarkdownRenderer md={md}></MarkdownRenderer>}></Route>
-      <Route path="/world" element={<p>world</p>}></Route>
+      <Route index element={<LandingPage/>}></Route>
+      <Route path="/edit" element={<EditorPage/>}></Route>
+      <Route path="/network" element={<NetworkPage/>}></Route>
     </Routes>
   )
 }
---
+++ src/pages/NetworkPage.tsx
@@ -0,0 +1,5 @@
+
+export default function NetworkPage() {
+
+    return <>Network</>;
+}
