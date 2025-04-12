e08abc50c047019e607974a77efe810ddd4db083
Ramy Kaddouri <myramkadd@gmail.com>
2025-04-11 23:35:27 -0400

    markdown latex rendering done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
+import MarkdownRenderer from './components/MarkdownRenderer'
 
 function App() {
+  const md = `# Header\n\nSome text\n\n**Yay math**: $\\frac{1}{x}$
+`
   return (
     <Routes>
-      <Route index element={<p>Hello</p>}></Route>
+      <Route index element={<MarkdownRenderer md={md}></MarkdownRenderer>}></Route>
       <Route path="/world" element={<p>world</p>}></Route>
     </Routes>
   )
--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,3 +1,20 @@
-export default function MarkdownRenderer() {
-  return <h1>foo</h1>
+import Markdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkParse from "remark-parse";
+
+type Props = {
+  md: string,
+}
+
+export default function MarkdownRenderer({ md }: Props) {
+  return <div className="prose prose-slate leading-snug my-4 prose-invert 
+            prose-headings:mt-5 prose-headings:mb-2 prose-ul:mt-0 
+            prose-pre:bg-transparent prose-pre:p-0 prose-li:my-1">
+    <Markdown
+      rehypePlugins={[rehypeKatex]}
+      remarkPlugins={[remarkMath, remarkParse]}>
+      {md}
+    </Markdown>
+  </div>
 }
