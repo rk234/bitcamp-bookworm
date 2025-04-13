ec152eae19f959d474467c8b8edbe626de47c277
Joel Chemmanur <joelchem@outlook.com>
2025-04-13 01:39:30 -0400

    byebye useless button

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -262,16 +262,6 @@ export default function Canvas({ className = "" }: CanvasProps) {
                 </svg>
                 Add circle
               </Button>
-              <Button variant="outline" className="rounded-full"
-                onClick={
-                  () => addLink()
-                }
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                Add Link
-              </Button>
             </>)}
         </div>
       </div>
